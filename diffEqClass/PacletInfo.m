64fd6a5e1c86a80e250ac1665212ce0e762f9523
mroge02 <mroge02@users.noreply.github.com>
2019-09-19 08:43:01 -0400

    Initial commit
    First uploading. Testing paclet development.

---
+++ diffEqClass/PacletInfo.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+Paclet[
+  Name        -> "diffEqClass",
+  Version     -> "0.2.1",
+  Creator     -> "mroge02 <mroge02@gmail.com>",
+  Description -> "Differential equations utilitiesw",
+  Extensions  -> {
+  {"Kernel", "Root"->".", "Context"->{"diffEqClass`"}},
+  {"FrontEnd"}
+  }
+  ]
